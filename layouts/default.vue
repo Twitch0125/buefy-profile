e6bbf9702ea5c3351971f83608d079d2ff9c0d79
Kaleb Ercanbrack <kaleb@pop-os.localdomain>
2019-09-27 14:20:45 -0600

    edit

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <!-- navbar -->
-    <b-navbar type="is-primary" shadow="true">
+    <b-navbar type="is-primary" shadow>
       <template slot="start">
         <b-navbar-item tag="router-link" :to="{path: '/'}">Home</b-navbar-item>
         <b-navbar-item tag="router-link" :to="{path: '/about'}">Who am I?</b-navbar-item>
